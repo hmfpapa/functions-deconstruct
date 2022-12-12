1cf4a9cec1dfb0c5c5db536bcc71e15a73ca031e
Hannah Papa <hmfloyd@gmail.com>
2022-12-12 09:30:04 -0600

    intial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+
+/*
+const collectSupplies = () => {
+    console.log("Collect supplies including rod, moss, netting, bucket, zip ties, and scissors")
+}
+
+const soakMoss = () => {
+    console.log("Soak moss in bucket of water")
+}
+
+const cutNetting = () => {
+    console.log("Cut netting to desired length, matching height of the rod")
+}
+
+const startNetting = () => {
+    console.log("zip tie end of netting to top and bottom of rod")
+}
+
+const addMoss = () => {
+    console.log("cover netting with moss, stacking into a small pile that spans the length of the rod")
+}
+
+const rollPole = () => {
+    console.log("starting with the pole, roll into the moss pulling the netting tightly")
+}
+
+const tieOff = () => {
+    console.log("zip tie the end of the netting to the inner layer of netting securing the moss inside")
+}
+
+collectSupplies()
+soakMoss()
+cutNetting()
+startNetting()
+addMoss()
+rollPole()
+tieOff()
+*/
+
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+let addResult = add(1, 2)
+
+const subtract = (firstNum, secondNum) => {
+    const diff = firstNum - secondNum
+    return diff
+}
+let subtractResult = subtract(6, 4)
+
+const multiply = (firstNumb, secondNumb) => {
+    const mult = firstNumb * secondNumb
+    return mult
+}
+let multResult = multiply(2, 3)
+
+const divide = (aNumber, anotherNumber) => {
+    const div = aNumber / anotherNumber
+    return div
+}
+let divResult = divide(60, 4)
+
+const square = (num1) => {
+    const sq = num1 * num1
+    return sq
+}
+let squareResult = square(4)
+
+console.log(addResult)
+console.log(subtractResult)
+console.log(multResult)
+console.log(divResult)
+console.log(squareResult)
+
+let animals = divide (60, 2.5)
+console.log (animals)
+
